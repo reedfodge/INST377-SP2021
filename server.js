9c233ff2937c7013d50442a133a051e5be124efd
rfodge <reedfodge@gmail.com>
2021-02-19 14:28:07 -0500

    (Most of) lab 4

--- server.js
+++ server.js
@@ -44,6 +44,8 @@ app.route('/api')
     console.log('Form data in res.body', req.body);
     console.log('Now send something back to your client');
     // res.json({data: dataToSendToFrontEnd});
+    console.log('hello world')
+    res.send('<p>hello world</p>')
   });
 
 app.listen(port, async () => {
